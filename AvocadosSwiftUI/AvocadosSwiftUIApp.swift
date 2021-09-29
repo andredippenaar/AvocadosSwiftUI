e1e9375dba1e0d79b7b71a9650dea28f8f9b985a
Andre Dippenaar <andre.pierre.dippenaar@gmail.com>
2021-09-29 14:43:33 +0200

    Initial Commit

---
+++ AvocadosSwiftUI/AvocadosSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosSwiftUIApp.swift
+//  AvocadosSwiftUI
+//
+//  Created by Andre Dippenaar on 2021/09/29.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
